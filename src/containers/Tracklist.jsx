d80ebd29b2b9184094682682711016bc8e11d040
Samuel Coleman <samuel.e.c519@gmail.com>
2023-06-12 21:57:40 -0600

    Change resultTracks and playlistTracks to be stateful

--- src/containers/Tracklist.jsx
+++ src/containers/Tracklist.jsx
@@ -2,42 +2,49 @@
 import SearchBar from '../components/SearchBar';
 import Playlist from '../components/Playlist';
 import SearchResults from '../components/SearchResults';
+import { useState } from 'react';
+
+
+
+const results = [
+    {
+        title: "100 Years",
+        artist: "Five For Fighting",
+        album: "The Battle for Everything",
+        id: 1
+    },
+    {
+        title: "1957",
+        artist: "Milo Greene",
+        album: "Milo Greene",
+        id: 2
+    },
+    {
+        title: "Medicine for Melancholy",
+        artist: "Rivers Cuomo",
+        album: "Medicine for Melancholy",
+        id: 3
+    }
+]
+const playlist = [
+    {
+        title: "All My Loving",
+        artist: "The Welll Pennies",
+        album: "Covers",
+        id: 4
+    }
+]
 
 function Tracklist() {
-    const results = [
-        {
-            title: "100 Years",
-            artist: "Five For Fighting",
-            album: "The Battle for Everything",
-            id: 1
-        },
-        {
-            title: "1957",
-            artist: "Milo Greene",
-            album: "Milo Greene",
-            id: 2
-        },
-        {
-            title: "Medicine for Melancholy",
-            artist: "Rivers Cuomo",
-            album: "Medicine for Melancholy",
-            id: 3
-        }
-    ]
-    const playlist = [
-        {
-            title: "All My Loving",
-            artist: "The Welll Pennies",
-            album: "Covers",
-            id: 4
-        }
-    ]
+    const [resultTracks, setResultTracks] = useState(results);
+    const [playlistTracks, setPlaylistTracks] = useState(playlist);
+
     return (
         <div className='Tracklist'>
             <SearchBar />
             <div className='lists-container'>
-                <SearchResults songArray={results}/>
-                <Playlist songArray={playlist}/>
+                <SearchResults songArray={resultTracks}/>
+                <Playlist songArray={playlistTracks}/>
             </div>
         </div>
     )
